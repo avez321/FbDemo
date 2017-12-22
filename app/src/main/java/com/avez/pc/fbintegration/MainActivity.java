3dab52f3d3fadd9392789e7c59276c4218e09c14
avez raj <avais.983@gmail.com>
2017-12-22 13:15:50 +0530

    twitter library added

--- app/src/main/java/com/avez/pc/fbintegration/MainActivity.java
+++ app/src/main/java/com/avez/pc/fbintegration/MainActivity.java
@@ -21,22 +21,22 @@ import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
-    private CallbackManager mCallbackManager;
-    private LoginButton mLoginButton;
-    private ProfilePictureView mProfilePictureView;
-    private TextView mName,mBirthdate,mLocation,mEmail;
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private ProfilePictureView profilePictureView;
+    private TextView name, birthdate, location, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mCallbackManager = CallbackManager.Factory.create();
+        callbackManager = CallbackManager.Factory.create();
 
         setContentView(R.layout.activity_main);
         findViewByIds();
-        mLoginButton = (LoginButton)findViewById(R.id.login_button);
-        mLoginButton.setReadPermissions(Constants.FB_REQUEST_PARMS);
-        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Constants.FB_REQUEST_PARMS);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 GraphRequest request = GraphRequest.newMeRequest(
@@ -44,26 +44,7 @@ public class MainActivity extends AppCompatActivity {
                         new GraphRequest.GraphJSONObjectCallback() {
                             @Override
                             public void onCompleted(JSONObject object, GraphResponse response) {
-                                Log.d("LoginActivity", response.toString());
-
-                                // Application code
-                                String email = object.optString("email");
-                                String birthday = object.optString("birthday");
-                                String name = object.optString("name");
-                                String id=object.optString("id");
-                                String location= null;
-                                try {
-                                    JSONObject jsonObject = object.getJSONObject("location");
-                                    location=jsonObject.optString("name");
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                                mEmail.setText("Email:"+email);
-                                mBirthdate.setText("Birthday:"+birthday);
-                                mName.setText("Name:"+name);
-                                mProfilePictureView.setProfileId(id);
-                                mLocation.setText("Location:"+location);
-
+                                registerCallBack(object, response);
                             }
                         });
                 Bundle parameters = new Bundle();
@@ -75,28 +56,50 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancel() {
-                mName.setText("Login attempt cancelled.");
+                name.setText("Login attempt cancelled.");
             }
 
             @Override
             public void onError(FacebookException e) {
-                mName.setText("Login attempt failed.");
+                name.setText("Login attempt failed.");
             }
         });
     }
 
+    private void registerCallBack(JSONObject object, GraphResponse response) {
+        Log.d("LoginActivity", response.toString());
+
+        // Application code
+        String email = object.optString("email");
+        String birthday = object.optString("birthday");
+        String name = object.optString("name");
+        String id=object.optString("id");
+        String location= null;
+        try {
+            JSONObject jsonObject = object.getJSONObject("location");
+            location=jsonObject.optString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        MainActivity.this.email.setText("Email:"+email);
+        birthdate.setText("Birthday:"+birthday);
+        MainActivity.this.name.setText("Name:"+name);
+        profilePictureView.setProfileId(id);
+        MainActivity.this.location.setText("Location:"+location);
+    }
+
     private void findViewByIds() {
-        mProfilePictureView= (ProfilePictureView) findViewById(R.id.profile_pic);
-        mName= (TextView) findViewById(R.id.name);
-        mBirthdate= (TextView) findViewById(R.id.birthdate);
-        mEmail= (TextView) findViewById(R.id.email);
-        mLocation= (TextView) findViewById(R.id.location);
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profile_pic);
+        name = (TextView) findViewById(R.id.name);
+        birthdate = (TextView) findViewById(R.id.birthdate);
+        email = (TextView) findViewById(R.id.email);
+        location = (TextView) findViewById(R.id.location);
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
 
